a6d5a6d585395ace6d7458cba0607fb9aa0a316a
YOUNSANG CHO <ysang10@gmail.com>
2021-11-28 20:13:18 +0900

    upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const port = 50000;
+
+//request callBack 방식
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
