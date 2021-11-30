026aea3a7d0eeadaed9e6b852d0917069606a6bf
YOUNSANG CHO <ysang10@gmail.com>
2021-11-30 13:58:54 +0900

    update

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const port = 50000;
 
 //request callBack 방식
 
-app.get('/', (req, res) => {
-    //console.log(req);
+app.post('/', (req, res) => {
+    console.log(req.body);
     res.send("Connected");
 })
 
+/* 
 app.post('/test', (req, res) => {
     console.log(req);
     res.send(res);
 })
+*/
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
