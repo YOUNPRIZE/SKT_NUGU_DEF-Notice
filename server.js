a0acbe44ce70b04a3ebaae001b7218e67b29014f
YOUNSANG CHO <ysang10@gmail.com>
2021-11-30 14:35:07 +0900

    update

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const request = require('request');
 const app = express();
 const port = 50000;
+const bodyParser = require('body-parser')
 
-//request callBack 방식
+
+app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
     console.log(req.body);
     res.send("Connected");
 })
 
-
+/*
 app.post('/test', (req, res) => {
     console.log(req.body);
     res.send(res);
 })
-
+*/
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
