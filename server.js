4f08a761b7b01d05653b699b4a627a384575d304
YOUNSANG CHO <ysang10@gmail.com>
2021-12-01 15:45:15 +0900

    update

--- server.js
+++ server.js
@@ -3,27 +3,73 @@ const request = require('request');
 const app = express();
 const port = 50000;
 const bodyParser = require('body-parser')
-
+const axios = require('axios').default;
+const url = 'https://api.odcloud.kr/api/15094782/v1/uddi:6b2017af-659d-437e-a549-c59788817675?page=1&perPage=120&serviceKey=SNt2RewTpkAADiy4foJQOIKaAZKkSA5afTUkvCxvaXAw9w9DATYShPP4NXV17v86%2B9BEM4n0v%2F2DOsVelv7wxg%3D%3D'
+const _ = require('lodash');
 
 app.use(bodyParser.json())
 
-
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.get('/', (req, res) => {
+    //console.log(req.body);
     res.send("Connected");
 })
 
+//axios Promise 방식
 
+const getData = function() {
+    // getData는 new Promise를 return한다.
+    return new Promise(function(resolve) {
+        //axios.get 자체도 Promise 함수이기 때문에 .then을 사용하는 것 같음.
+        axios.post(url)
+        .then(function(response) {
+            resolve(response.data);
+        });
+    });
+}
+/*
+app.post('/defAPI', (req, res) => {
+    const result = async function () {
+        const result2 = await getData()
+        .then(function(response) {
+            res.send(response);
+            const extract = response.data
+            const extract_data = _.filter(extract, function (o) {return o.재고량 > 0});
+            //console.log(extract_data)
+            const extractAdd = extract_data.filter(object => {
+                if (object.주소.indexOf(req.body.action.parameters.DEF_LOC.value) > -1) {
+                    return object;
+                }
+                return null;
+            });
+            console.log(extractAdd)
+        });
+        }
+    result()
+})
+*/
 app.post('/test', (req, res) => {
-    console.log(req.body.action.parameters.DEF_LOC.value);
-    //console.log(req.body)
-
-
+    const result = async function () {
+        const result2 = await getData()
+        .then(function(response) {
+            res.send(response);
+            const extract = response.data
+            const extract_data = _.filter(extract, function (o) {return o.재고량 > 0});
+            //console.log(extract_data)
+            const extractAdd = extract_data.filter(object => {
+                if (object.주소.indexOf(req.body.action.parameters.DEF_LOC.value) > -1) {
+                    return object;
+                }
+                return null;
+            });
+            //console.log(extractAdd)
+        });
+        }
+    result()
     req.body.resultCode = "OK";
     req.body.output = {
         DEF_LOC: req.body.action.parameters.DEF_LOC.value,
-        DEF_API_ADD: "위치",
-        DEF_API_NUM: "번호",
+        DEF_API_ADD: /*"위치"*/extractAdd[0].주소,
+        DEF_API_NUM: /*"번호"*/extractAdd[0].번호
     }
 
     /*
