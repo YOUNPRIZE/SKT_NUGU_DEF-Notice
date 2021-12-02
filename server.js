b194c759cb72c56fb18e4b0fcce7bc9daddd74c8
YOUNSANG CHO <ysang10@gmail.com>
2021-12-02 15:34:10 +0900

    update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 50000;
 const bodyParser = require('body-parser')
 const axios = require('axios').default;
 const url = 'https://api.odcloud.kr/api/15094782/v1/uddi:6b2017af-659d-437e-a549-c59788817675?page=1&perPage=120&serviceKey=SNt2RewTpkAADiy4foJQOIKaAZKkSA5afTUkvCxvaXAw9w9DATYShPP4NXV17v86%2B9BEM4n0v%2F2DOsVelv7wxg%3D%3D'
+const url_update = 'https://api.odcloud.kr/api/uws/v1/inventory?page=1&perPage=120&serviceKey=SNt2RewTpkAADiy4foJQOIKaAZKkSA5afTUkvCxvaXAw9w9DATYShPP4NXV17v86%2B9BEM4n0v%2F2DOsVelv7wxg%3D%3D'
 const _ = require('lodash');
 
 app.use(bodyParser.json())
@@ -16,7 +17,7 @@ app.get('/', (req, res) => {
 
 app.post('/test', (nuguReq, nuguRes) => {
     //console.log(nuguReq.body)
-    axios.get(url)
+    axios.get(url_update)
     .then((apiRes) => {
         const extract = apiRes.data.data;
         const extract_data = _.filter(extract, function (o) {return o.inventory > 0})
