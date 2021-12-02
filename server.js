c0d90e720570b1c3fd4442f9f348a90612d6b353
YOUNSANG CHO <ysang10@gmail.com>
2021-12-02 15:31:21 +0900

    update

--- server.js
+++ server.js
@@ -14,66 +14,22 @@ app.get('/', (req, res) => {
     res.send("Connected");
 })
 
-//axios Promise 방식
-
-// const getData = function() {
-//     // getData는 new Promise를 return한다.
-//     return new Promise(function(resolve) {
-//         //axios.get 자체도 Promise 함수이기 때문에 .then을 사용하는 것 같음.
-//         axios.get(url)
-//         .then(function(response) {
-//             resolve(response.data);
-//         });
-//     });
-// }
-
-// app.post('/test', (req, res) => {
-//     getData().then(function(response) {
-//         console.log(request);
-//         const extract = response.data;
-//         const extract_data = _.filter(extract, function (o) {return o.재고량 > 0});
-//         const extractAdd = _.filter(extract_data, function (o) {return o.주소.indexOf('안성') > -1});
-//         //res.send(extractAdd[0].주소);
-//     })
-    
-//     req.body.resultCode = "OK";
-//     req.body.output = {
-//         DEF_LOC: req.body.action.parameters.DEF_LOC.value,
-//         DEF_API_ADD: "위치"/*result()[0].주소*/,
-//         DEF_API_NUM: "번호"/*result()[0].번호*/
-//     }
-//     res.send(req.body);
-//     res.end;
-// })
-
 app.post('/test', (nuguReq, nuguRes) => {
     //console.log(nuguReq.body)
     axios.get(url)
     .then((apiRes) => {
         const extract = apiRes.data.data;
-        const extract_data = _.filter(extract, function (o) {return o.재고량 > 0})
-        const extractAdd = _.filter(extract_data, function (o) {return o.주소.indexOf('안성') > -1})
+        const extract_data = _.filter(extract, function (o) {return o.inventory > 0})
+        const extractAdd = _.filter(extract_data, function (o) {return o.주소.indexOf(nuguReq.body.action.parameters.DEF_LOC.value) > -1})
         nuguReq.body.resultCode = "OK";
         nuguReq.body.output = {
             DEF_LOC: nuguReq.body.action.parameters.DEF_LOC.value,
-            DEF_API_ADD: /*"위치"*/extractAdd[0].주소,
-            DEF_API_NUM: /*"번호"*/extractAdd[0].전화번호
+            DEF_API_ADD: /*"주소"*/extractAdd[0].addr,
+            DEF_API_NUM: /*"번호"*/extractAdd[0].tel
         }
         nuguRes.send(nuguReq.body)
     })
 })
-
-/*
-    req.body.resultCode = "OK";
-    req.body.output = {
-        DEF_LOC: req.body.action.parameters.DEF_LOC.value,
-        DEF_API_ADD: "위치"/*result()[0].주소*//*,
-        DEF_API_NUM: "번호"/*result()[0].번호*//*
-    }
-    res.send(req.body);
-    res.end;
-})
-*/
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
