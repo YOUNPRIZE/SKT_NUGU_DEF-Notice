efa6ce4dea610d5aec475f635ac41220852de9e9
YOUNSANG CHO <ysang10@gmail.com>
2021-12-04 15:06:02 +0900

    update

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
 const express = require('express');
-const request = require('request');
 const app = express();
-const port = 50000;
-const bodyParser = require('body-parser')
 const axios = require('axios').default;
-const url = 'https://api.odcloud.kr/api/15094782/v1/uddi:6b2017af-659d-437e-a549-c59788817675?page=1&perPage=120&serviceKey=SNt2RewTpkAADiy4foJQOIKaAZKkSA5afTUkvCxvaXAw9w9DATYShPP4NXV17v86%2B9BEM4n0v%2F2DOsVelv7wxg%3D%3D'
-const url_update = 'https://api.odcloud.kr/api/uws/v1/inventory?page=1&perPage=120&serviceKey=SNt2RewTpkAADiy4foJQOIKaAZKkSA5afTUkvCxvaXAw9w9DATYShPP4NXV17v86%2B9BEM4n0v%2F2DOsVelv7wxg%3D%3D'
+const bodyParser = require('body-parser')
 const _ = require('lodash');
+const port = 50000;
+const url = 'https://api.odcloud.kr/api/uws/v1/inventory?page=1&perPage=120&serviceKey=SNt2RewTpkAADiy4foJQOIKaAZKkSA5afTUkvCxvaXAw9w9DATYShPP4NXV17v86%2B9BEM4n0v%2F2DOsVelv7wxg%3D%3D'
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    //console.log(req.body);
     res.send("Connected");
 })
 
 app.post('/test', (nuguReq, nuguRes) => {
-    //console.log(nuguReq.body)
-    axios.get(url_update)
+    axios.get(url)
     .then((apiRes) => {
-        console.log(nuguReq.body.action.parameters.DEF_LOC.value)
+        //JSON 에서 date 값만 추출
         const extract = apiRes.data.data;
+        
+        //요소수 재고량이 0이상인 값만 재추출
         const extract_data = _.filter(extract, function (o) {return o.inventory > 0})
+        
+        //nuguReq.body.action.parameters.DEF_LOC.value => User가 request한 지명만 추출
         const extractAdd = _.filter(extract_data, function (o) {return o.addr.indexOf(nuguReq.body.action.parameters.DEF_LOC.value) > -1})
+        
+        //필요한 response 값 입력
         nuguReq.body.resultCode = "OK";
+        
+        //필요한 response 값 입력
         nuguReq.body.output = {
+            //지명
             DEF_LOC: nuguReq.body.action.parameters.DEF_LOC.value,
-            DEF_API_ADD: /*"주소"*/extractAdd[0].addr,
-            DEF_API_NUM: /*"번호"*/extractAdd[0].tel
+            
+            //주소
+            DEF_API_ADD: extractAdd[0].addr,
+            
+            //전화번호
+            DEF_API_NUM: extractAdd[0].tel
         }
         nuguRes.send(nuguReq.body)
     })
