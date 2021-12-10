e54b61d6d61fa99bc05cb23a84ce23fd447a0e3e
YOUNSANG CHO <ysang10@gmail.com>
2021-12-10 15:48:26 +0900

    update

--- server.js
+++ server.js
@@ -19,29 +19,53 @@ app.post('/test', (nuguReq, nuguRes) => {
         const extract = apiRes.data.data;
         
         //요소수 재고량이 0이상인 값만 재추출
-        const extract_data = _.filter(extract, function (o) {return o.inventory > 0})
-        
-        //nuguReq.body.action.parameters.DEF_LOC.value => User가 request한 지명만 추출
-        const extractAdd = _.filter(extract_data, function (o) {return o.addr.indexOf(nuguReq.body.action.parameters.DEF_LOC.value) > -1})
-        
-        //필요한 response 값 입력
-        nuguReq.body.resultCode = "OK";
-        
-        //필요한 response 값 입력
-        nuguReq.body.output = {
-            //지명
-            DEF_LOC: nuguReq.body.action.parameters.DEF_LOC.value,
-            
-            //주소
-            DEF_API_ADD: extractAdd[0].addr,
-            
-            //전화번호
-            DEF_API_NUM: extractAdd[0].tel
-        }
-        nuguRes.send(nuguReq.body)
+        const extract_data = _.filter(extract, function (o) {
+            return o.inventory > 0
+        })
+
+        navigator.geolocation.getCurrentPosition(function(success, error) {
+            if (error) {
+                console.error(error);
+            }
+            console.log(success.latitude);
+            const addInfo = []
+            for (let i of JSON.parse(body).data) {
+                i.distancefromcurrentlocation = getDistance(success.latitude, success.longitude, i.lat, i.lng);
+                addInfo.push(i);
+            };
+            const newSuccess = _.sortBy(addInfo, "distancefromcurrentlocation");  
+
+            //필요한 response 값 입력
+            nuguReq.body.resultCode = "OK";  
+            //필요한 response 값 입력
+            nuguReq.body.output = {
+                //지명
+                DEF_LOC: nuguReq.body.action.parameters.DEF_LOC.value,
+                
+                //주소
+                DEF_API_ADD: newSuccess[0].addr,
+                
+                //전화번호
+                DEF_API_NUM: newSuccess[0].tel
+            }
+            nuguRes.send(nuguReq.body)
+        })
     })
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
+
+const getDistance = function(lat1,lng1,lat2,lng2) { 
+    function deg2rad(deg) { 
+        return deg * (Math.PI/180) 
+    } 
+    var R = 6371; // Radius of the earth in km 
+    var dLat = deg2rad(lat2-lat1); // deg2rad below 
+    var dLon = deg2rad(lng2-lng1); 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km return d; 
+    return d;
+};
