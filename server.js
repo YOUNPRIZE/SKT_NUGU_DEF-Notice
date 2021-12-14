c02c94283e1cb1e9e98dc1d4e9dfe8708fc9fcc2
YOUNSANG CHO <ysang10@gmail.com>
2021-12-14 15:41:32 +0900

    update

--- server.js
+++ server.js
@@ -25,11 +25,34 @@ app.post('/test', (nuguReq, nuguRes) => {
             return o.inventory > 0
         })
 
+        //nuguReq.body.action.parameters.DEF_LOC.value => User가 request한 지명만 추출
+        const extractAdd = _.filter(extract_data, function (o) {return o.addr.indexOf(nuguReq.body.action.parameters.DEF_LOC.value) > -1})
+
+        //필요한 response 값 입력
+        nuguReq.body.resultCode = "OK";
+
+        //필요한 response 값 입력
+        nuguReq.body.output = {
+            //지명
+            DEF_LOC: nuguReq.body.action.parameters.DEF_LOC.value,
+
+            //주소
+            DEF_API_ADD: extractAdd[0].addr,
+
+            //전화번호
+            DEF_API_NUM: extractAdd[0].tel
+        }
+        nuguRes.send(nuguReq.body)
+    })
+})
+
+//
+/*
         navigator.geolocation.getCurrentPosition(function(success, error) {
             if (error) {
                 console.error(error);
             }
-            console.log(success.latitude, success.longitude);
+            console.log("현재 위도는", success.latitude,", 경도는", success.longitude,"입니다.");
             const addInfo = []
             for (let i of extract_data) {
                 i.distancefromcurrentlocation = getDistance(success.latitude, success.longitude, i.lat, i.lng);
@@ -52,14 +75,14 @@ app.post('/test', (nuguReq, nuguRes) => {
             }
             nuguRes.send(nuguReq.body)
         })
-    })
-})
+*/
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
 
-//동기 함수
+/*
+//동기 함수 => 함수의 형태를 보고 다른 곳에서 불러오는 함수가 있으면 비동기 함수이다.
 const getDistance = function(lat1,lng1,lat2,lng2) { 
     function deg2rad(deg) { 
         return deg * (Math.PI/180) 
@@ -72,3 +95,4 @@ const getDistance = function(lat1,lng1,lat2,lng2) {
     var d = R * c; // Distance in km return d; 
     return d;
 };
+*/
