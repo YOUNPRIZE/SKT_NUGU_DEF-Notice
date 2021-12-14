351b5f0dcd0105667c2be48dd75068c41dcdb2e2
YOUNSANG CHO <ysang10@gmail.com>
2021-12-14 16:13:32 +0900

    update

--- server.js
+++ server.js
@@ -24,7 +24,28 @@ app.post('/test', (nuguReq, nuguRes) => {
         const extract_data = _.filter(extract, function (o) {
             return o.inventory > 0
         })
-        
+
+        //nuguReq.body.action.parameters.DEF_LOC.value => User가 request한 지명만 추출
+        const extractAdd = _.filter(extract_data, function (o) {return o.addr.indexOf(nuguReq.body.action.parameters.DEF_LOC.value) > -1})
+
+        //필요한 response 값 입력
+        nuguReq.body.resultCode = "OK";  
+        //필요한 response 값 입력
+        nuguReq.body.output = {
+            //지명
+            DEF_LOC: nuguReq.body.action.parameters.DEF_LOC.value,
+            
+            //주소
+            DEF_API_ADD: extractAdd[0].addr,
+            
+            //전화번호
+            DEF_API_NUM: extractAdd[0].tel
+        }
+        nuguRes.send(nuguReq.body)
+    })
+})
+
+/*
         navigator.geolocation.getCurrentPosition(function(success, error) {
             if (error) {
                 console.error(error);
@@ -36,30 +57,13 @@ app.post('/test', (nuguReq, nuguRes) => {
                 addInfo.push(i);
             };
             const newSuccess = _.sortBy(addInfo, "distancefromcurrentlocation");  
-
-            //필요한 response 값 입력
-            nuguReq.body.resultCode = "OK";  
-            //필요한 response 값 입력
-            nuguReq.body.output = {
-                //지명
-                DEF_LOC: nuguReq.body.action.parameters.DEF_LOC.value,
-                
-                //주소
-                DEF_API_ADD: newSuccess[0].addr,
-                
-                //전화번호
-                DEF_API_NUM: newSuccess[0].tel
-            }
-            nuguRes.send(nuguReq.body)
-        })
-    })
-})
+*/
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
 
-//동기 함수
+/*
 const getDistance = function(lat1,lng1,lat2,lng2) { 
     function deg2rad(deg) { 
         return deg * (Math.PI/180) 
@@ -72,3 +76,4 @@ const getDistance = function(lat1,lng1,lat2,lng2) {
     var d = R * c; // Distance in km return d; 
     return d;
 };
+*/
